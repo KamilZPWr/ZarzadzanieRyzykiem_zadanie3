4a984a33eb1fbec7946cea1428e1062c08a374ca
Wojtek <wojtek.zulawinski@gmail.com>
2018-04-24 00:02:18 +0200

    cut 8 first, fixed rates

--- single_index_model.m
+++ single_index_model.m
@@ -8,15 +8,19 @@ ural_price = oil_data(:,2);
 R_brent =(brent_price(2:end)-brent_price(1:end-1))./brent_price(1:end-1)*100;
 R_ural = (ural_price(2:end)-ural_price(1:end-1))./ural_price(1:end-1)*100;
 
+R_brent = R_brent(9:end);  
+R_ural = R_ural(9:end);
+
 var_brent = var(R_brent);
 var_ural = var(R_ural);
 
 std_brent = sqrt(var_brent);
 std_ural = sqrt(var_ural);
 
-cov_brent_ural = max(cov(R_brent, R_ural));
+%cov_brent_ural = max(cov(R_brent, R_ural)); 
 
 R_index = (index_price(2:end)-index_price(1:end-1))./index_price(1:end-1)*100;
+R_index = R_index(9:end);
 
 beta_brent = sum((R_brent - mean(R_brent)).*(R_index-mean(R_index)))/sum((R_index-mean(R_index)).^2)
 beta_ural = sum((R_ural - mean(R_ural)).*(R_index-mean(R_index)))/sum((R_index-mean(R_index)).^2)
@@ -61,15 +65,22 @@ end
 weight_brent_min_risk = (var(R_index)*beta_ural*(beta_ural-beta_brent)+var_random_fractor_ural)/(var(R_index)*(beta_brent-beta_ural).^2+var_random_fractor_brent+var_random_fractor_ural)
 weight_ural_min_risk = 1 - weight_brent_min_risk
 
+figure
+w = 0:0.001:1;
+sigma_p_w = (w*beta_brent+(1-w)*beta_ural).^2*var(R_index)+w.^2*var_random_fractor_brent+(1-w).^2*var_random_fractor_ural;
+sigma_p_wbmr = (weight_brent_min_risk*beta_brent+(1-weight_brent_min_risk)*beta_ural).^2*var(R_index)+weight_brent_min_risk.^2*var_random_fractor_brent+(1-weight_brent_min_risk).^2*var_random_fractor_ural
+plot(w,sigma_p_w)
+hold on
+plot(weight_brent_min_risk, sigma_p_wbmr,'.','markersize',20)
 
 % 1d)
 
-fixedrate = 0.05;
+fixedrate = 0.8;
 weight_brent_fixedrate = (fixedrate-mean(R_ural))/(mean(R_brent)-mean(R_ural))
 weight_ural_fixedrate = 1-weight_brent_fixedrate
 
 figure
-w = -1:0.01:1;
+w = 0:0.001:1;
 sigma_p_w = (w*beta_brent+(1-w)*beta_ural).^2*var(R_index)+w.^2*var_random_fractor_brent+(1-w).^2*var_random_fractor_ural;
 sigma_p_wbmr = (weight_brent_min_risk*beta_brent+(1-weight_brent_min_risk)*beta_ural).^2*var(R_index)+weight_brent_min_risk.^2*var_random_fractor_brent+(1-weight_brent_min_risk).^2*var_random_fractor_ural
 plot(w,sigma_p_w)
@@ -78,9 +89,9 @@ plot(weight_brent_min_risk, sigma_p_wbmr,'.')
 
 % 1e)
 
-riskfree_rate = 0.02;
+riskfree_rate = 0.175;
 
-w = 0:0.1:1;
+w = 0:0.001:1;
 sharps_index = 0;
 market_portfolio_brent_weight = 0;
 for i = w
@@ -98,10 +109,11 @@ sharps_index
 market_portfolio_brent_weight
 market_portfolio_ural_weight
 
-w = 0:0.1:1;
+w = 0:0.001:1;
 beta_p = w*beta_brent + (1-w)*beta_ural;
 E_p = w*alfa_brent + (1-w)*alfa_ural + mean(R_index) * beta_p;
 Var_p = beta_p.^2 * var(R_index) + w.^2*var_random_fractor_brent + (1-w).^2*var_random_fractor_ural;
 sharpe_index = (E_p - riskfree_rate)./sqrt(Var_p);
 figure 
 plot(w,sharpe_index)
+
