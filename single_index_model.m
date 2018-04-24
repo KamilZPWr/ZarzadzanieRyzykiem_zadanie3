b3fc6a6b4c0a8b40f75e44e20503d18923d83fe6
Wojtek <wojtek.zulawinski@gmail.com>
2018-04-24 12:25:55 +0200

    add plots

--- single_index_model.m
+++ single_index_model.m
@@ -11,6 +11,7 @@ R_ural = (ural_price(2:end)-ural_price(1:end-1))./ural_price(1:end-1)*100;
 R_brent = R_brent(9:end);  
 R_ural = R_ural(9:end);
 
+
 var_brent = var(R_brent);
 var_ural = var(R_ural);
 
@@ -21,6 +22,7 @@ std_ural = sqrt(var_ural);
 
 R_index = (index_price(2:end)-index_price(1:end-1))./index_price(1:end-1)*100;
 R_index = R_index(9:end);
+
 
 beta_brent = sum((R_brent - mean(R_brent)).*(R_index-mean(R_index)))/sum((R_index-mean(R_index)).^2)
 beta_ural = sum((R_ural - mean(R_ural)).*(R_index-mean(R_index)))/sum((R_index-mean(R_index)).^2)
@@ -34,11 +36,27 @@ var_random_fractor_ural = var(R_ural - (alfa_ural + beta_ural.*R_index))
 sharps_index_brent = (mean(R_brent) - mean(R_index))/sqrt(var(R_brent))
 sharps_index_ural = (mean(R_ural) - mean(R_index))/sqrt(var(R_ural))
 
-coefficient_of_determination_brent = beta_brent*var(R_index)/var(R_brent)
-coefficient_of_determination_ural = beta_ural*var(R_index)/var(R_ural)
+coefficient_of_determination_brent = beta_brent^2*var(R_index)/var(R_brent)
+coefficient_of_determination_ural = beta_ural^2*var(R_index)/var(R_ural)
 
 weight_brent = 0.86
 weight_ural = 0.14
+
+figure
+plot(R_index,R_brent,'.');
+hold on
+t=linspace(min(R_index),max(R_index),1000);
+plot(t,alfa_brent+beta_brent*t)
+xlabel('Stopa zwrotu indeksu')
+ylabel('Stopa zwrotu Brent')
+
+figure
+plot(R_index,R_ural,'.');
+hold on
+t=linspace(min(R_index),max(R_index),1000);
+plot(t,alfa_ural+beta_ural*t)
+xlabel('Stopa zwrotu indeksu')
+ylabel('Stopa zwrotu Ural')
 
 % 1a)
 beta_p0 = weight_brent*beta_brent + weight_ural*beta_ural;
@@ -72,6 +90,8 @@ sigma_p_wbmr = (weight_brent_min_risk*beta_brent+(1-weight_brent_min_risk)*beta_
 plot(w,sigma_p_w)
 hold on
 plot(weight_brent_min_risk, sigma_p_wbmr,'.','markersize',20)
+ylabel('Wariancja portfela')
+xlabel('Waga brent')
 
 % 1d)
 
@@ -80,18 +100,25 @@ weight_brent_fixedrate = (fixedrate-mean(R_ural))/(mean(R_brent)-mean(R_ural))
 weight_ural_fixedrate = 1-weight_brent_fixedrate
 
 figure
-w = 0:0.001:1;
+w = 0:0.001:weight_brent_fixedrate;
 sigma_p_w = (w*beta_brent+(1-w)*beta_ural).^2*var(R_index)+w.^2*var_random_fractor_brent+(1-w).^2*var_random_fractor_ural;
-sigma_p_wbmr = (weight_brent_min_risk*beta_brent+(1-weight_brent_min_risk)*beta_ural).^2*var(R_index)+weight_brent_min_risk.^2*var_random_fractor_brent+(1-weight_brent_min_risk).^2*var_random_fractor_ural
-plot(w,sigma_p_w)
+sigma_p_wbfr = (weight_brent_fixedrate*beta_brent+(1-weight_brent_fixedrate)*beta_ural).^2*var(R_index)+weight_brent_fixedrate.^2*var_random_fractor_brent+(1-weight_brent_fixedrate).^2*var_random_fractor_ural
+plot(w,sigma_p_w,'r')
+w = weight_brent_fixedrate:0.001:1;
+sigma_p_w = (w*beta_brent+(1-w)*beta_ural).^2*var(R_index)+w.^2*var_random_fractor_brent+(1-w).^2*var_random_fractor_ural;
 hold on
-plot(weight_brent_min_risk, sigma_p_wbmr,'.')
+plot(w,sigma_p_w,'b')
+plot(weight_brent_fixedrate, sigma_p_wbfr,'r.','markersize',20)
+
+plot(0*[50:0.01:70]+weight_brent_fixedrate,[50:0.01:70],'r--')
+ylabel('Wariancja portfela')
+xlabel('Waga brent')
 
 % 1e)
 
 riskfree_rate = 0.175;
 
-w = 0:0.001:1;
+w = 0:0.0001:1;
 sharps_index = 0;
 market_portfolio_brent_weight = 0;
 for i = w
@@ -109,11 +136,14 @@ sharps_index
 market_portfolio_brent_weight
 market_portfolio_ural_weight
 
-w = 0:0.001:1;
+w = 0:0.0001:1;
 beta_p = w*beta_brent + (1-w)*beta_ural;
 E_p = w*alfa_brent + (1-w)*alfa_ural + mean(R_index) * beta_p;
 Var_p = beta_p.^2 * var(R_index) + w.^2*var_random_fractor_brent + (1-w).^2*var_random_fractor_ural;
 sharpe_index = (E_p - riskfree_rate)./sqrt(Var_p);
 figure 
 plot(w,sharpe_index)
-
+hold on
+plot(market_portfolio_brent_weight,sharps_index,'r.','markersize',20)
+ylabel('Indeks Sharpe`a')
+xlabel('Waga brent')
